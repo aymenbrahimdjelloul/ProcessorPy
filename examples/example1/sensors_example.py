921088686b787f467a0e804feed6e649de13f52f
Aymen Brahim Djelloul <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2023-08-12 06:54:27 -0700

    Create sensors_example.py

---
+++ examples/example1/sensors_example.py
@@ -0,0 +1,17 @@
+# Author : Aymen Brahim Djelloul
+# Date : 12.08.2023
+# This file is an example for getting the sensros-readings from Sensors class in ProcessorPy
+
+# Import Sensors class from ProcessorPy
+from ProcessorPy import Sensors
+
+# Create Sensors object class
+sense = Sensors()
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
