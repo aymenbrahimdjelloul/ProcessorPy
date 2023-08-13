368719a431511636661f87029e37c5756fa33180
Aymen Brahim Djelloul <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2023-08-13 19:54:07 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,36 @@
+from processor_py import ProcessorPy
+import platform
+
+my_cpu = ProcessorPy.Processor()
+sensors = ProcessorPy.Sensors()
+
+
+def processor_py_test():
+    """ This function will test ProcessorPy and return the testing result """
+
+    # First test Processor Class if it works properly
+    cpu_info_dict: dict = my_cpu.get_cpu_info()
+
+    # Check if there is None value in CPU info dictionary
+    for value in cpu_info_dict.values():
+
+        if value is None:
+            return False
+
+    # Check sensors result
+    sensors_result = (sensors.get_cpu_clock_speed(), sensors.get_cpu_usage(), sensors.get_cpu_voltage())
+    for sensor in sensors_result:
+
+        if sensor is None:
+            return False
+
+    return True
+
+
+if processor_py_test():
+    print(f"ProcessorPy is working properly on {platform.freedesktop_os_release()['PRETTY_NAME']} \n"
+          f"with {my_cpu.name} CPU ")
+
+else:
+    print(f"ProcessorPy Failed to run properly on {platform.freedesktop_os_release()['PRETTY_NAME']}\n"
+          f"with {my_cpu.name} CPU")
