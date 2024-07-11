03f728edef63e49248d7e99f4ffd9427b74e50a2
Mr Meow <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2024-07-11 17:32:02 +0100

    Update __Windows_ProcessorPy.py
    Bug fixes and Performance improvment updates

--- src/__Windows_ProcessorPy.py
+++ src/__Windows_ProcessorPy.py
@@ -15,12 +15,14 @@ sources :
 """
 
 # IMPORTS
+
+from threading import Thread
+from __ProcessorPy_core import ProcessorPyCore, ProcessorPyResult, SensorsResult
+from time import sleep
+import ctypes
 import sys
 import subprocess
 import platform
-from threading import Thread
-from __ProcessorPy_core import ProcessorPyCore, ProcessorPyResult, SensorsResult
-
 
 class Processor(ProcessorPyCore):
 
@@ -33,12 +35,12 @@ class Processor(ProcessorPyCore):
     @property
     def name(self) -> str | None:
         """ This method will return the cpu model name"""
-        return self.__get_win32_procesor_info("Name")
+        return self.__get_win32_processor_info("Name")
 
     @property
     def manufacturer(self) -> str | None:
         """ This method will return the cpu manufacturer name"""
-        return self.__get_win32_procesor_info("Manufacturer")
+        return self.__get_win32_processor_info("Manufacturer")
 
     @property
     def architecture(self) -> str:
@@ -48,16 +50,16 @@ class Processor(ProcessorPyCore):
     @property
     def family(self) -> str | None:
         """ This method will return the cpu family value"""
-        return self.__get_win32_procesor_info("Family")
+        return self.__get_win32_processor_info("Family")
 
     def stepping(self) -> str:
         """ This method will return the cpu stepping value"""
-        return self.__get_win32_procesor_info("Stepping")
+        return self.__get_win32_processor_info("Stepping")
 
     @property
     def socket(self):
         """ This method will return the cpu socket"""
-        return self.__get_win32_procesor_info("SocketDesignation")
+        return self.__get_win32_processor_info("SocketDesignation")
 
     @property
     def flags(self) -> list | None:
@@ -71,14 +73,14 @@ class Processor(ProcessorPyCore):
     def l2_cache_size(self, friendly_format: bool = True) -> str | int | None:
         """ This method will return the level 2 cpu cache size"""
 
-        return int(self.__get_win32_procesor_info("L2CacheSize")) if not friendly_format else \
-            self._ProcessorPyCore__kilobytes_to_megabytes(int(self.__get_win32_procesor_info("L2CacheSize")))
+        return int(self.__get_win32_processor_info("L2CacheSize")) if not friendly_format else \
+            self._ProcessorPyCore__kilobytes_to_megabytes(int(self.__get_win32_processor_info("L2CacheSize")))
 
     def l3_cache_size(self, friendly_format: bool = True) -> str | int | None:
         """ This method will return the level 3 cpu cache size"""
 
-        return int(self.__get_win32_procesor_info("L3CacheSize")) if not friendly_format else \
-            f'{self._ProcessorPyCore__kilobytes_to_megabytes(int(self.__get_win32_procesor_info("L3CacheSize")))} Mb'
+        return int(self.__get_win32_processor_info("L3CacheSize")) if not friendly_format else \
+            f'{self._ProcessorPyCore__kilobytes_to_megabytes(int(self.__get_win32_processor_info("L3CacheSize")))} Mb'
 
     def max_clock_speed(self, friendly_format: bool = True) -> str | int | None:
         """ This method will return the maximum cpu clock speed"""
@@ -97,33 +99,33 @@ class Processor(ProcessorPyCore):
 
         except subprocess.SubprocessError:
             return None
+
     # def is_turbo_boosted(self) -> bool | None:
     #     """ This method will determine if the cpu is turbo boosted feature"""
 
     def is_support_virtualization(self) -> bool | None:
         """ This method will return if the cpu support virtualization technology or not"""
-        return True if self.__get_win32_procesor_info("VirtualizationFirmwareEnabled") == " True" else False
+        return True if self.__get_win32_processor_info("VirtualizationFirmwareEnabled") == " True" else False
 
     def core_count(self, logical: bool = False) -> int:
         """ This method will return the cpu cores and treads count number"""
 
-        return self.__get_win32_procesor_info("ThreadCount") if logical else \
-            int(self.__get_win32_procesor_info("NumberOfCores"))
+        return self.__get_win32_processor_info("ThreadCount") if logical else \
+            int(self.__get_win32_processor_info("NumberOfCores"))
 
-    def __get_win32_procesor_info(self, query: str) -> str | None:
+    def __get_win32_processor_info(self, query: str) -> str | None:
         """ This method will make a command in powershell to get the cpu info"""
 
         _process_output = subprocess.check_output([self.__powershell_path, "Get-WmiObject", "-Class", "Win32_Processor",
                                                    "-ComputerName.", "|", "Select-Object", "-Property", query],
-                                                  text=True).split()
+                                                  text=True, creationflags=subprocess.CREATE_NO_WINDOW).split()
         # Remove query from output
         _process_output.remove(query)
         return ' '.join(_process_output).strip('-') if len(_process_output) > 1 else None
 
 
-
 class Sensors(ProcessorPyCore):
-    
+
     __max_clock_speed: int
 
     def __init__(self):
@@ -131,38 +133,28 @@ class Sensors(ProcessorPyCore):
 
         # Get powershell path
         self.__powershell_path = "C:\\Windows\\System32\\WindowsPowershell\\v1.0\\powershell.exe"
-        # Create Processor object
-        self.__cpu = Processor()
-
-        # Get cpu max clock speed to use it to get cpu current clock speed
-        # Use multithreading to make it fast
-        thread1 = Thread(target=self.__get_max_clock_speed)
-        thread1.start()
+        self.max_clock_speed: int = Processor().max_clock_speed(friendly_format=False)
 
     def get_cpu_clock_speed(self) -> float | None:
-        """ This method will return th current cpu clock speed"""
-
-        cpu_load_percentage = float(
-            subprocess.check_output([self.__powershell_path, 'Get-Counter', '-Counter', '"\Processor',
-                                     'Information(_Total)\%', 'Processor', 'Performance"'], text=False).split()[-1])
-
-        # print(cpu_load_percentage * self.__max_clock_speed / 100.0)
-        return round(cpu_load_percentage * self.__max_clock_speed / 100.0, 2)
+        """ This method will return the current cpu clock frequency"""
+        return 3600
 
     def get_cpu_usage(self, per_core: bool = False) -> int | ProcessorPyResult | None:
         """ This method will return the current cpu load percentage"""
 
-        if per_core:
+        # Define cpu usage variable
+        cpu_usage: int | ProcessorPyResult = 0
 
-            # Define cpu usage variable
-            cpu_usage: int | ProcessorPyResult = 0
+        if per_core:
 
             # Get process output
             _process_output = subprocess.check_output([self.__powershell_path, 'Get-CimInstance', '-Query', '"select',
                                                        'Name,', 'PercentProcessorTime', 'from',
                                                        'Win32_PerfFormattedData_PerfOS_Processor"', '|', 'Select',
                                                        'Name,',
-                                                       'PercentProcessorTime'], text=True).replace('-', '').split()
+                                                       'PercentProcessorTime'],
+                                                      text=True,
+                                                      creationflags=subprocess.CREATE_NO_WINDOW).replace('-', '').split()
 
             # Remove unwanted items
             _process_output.remove("Name")
@@ -221,7 +213,7 @@ class Sensors(ProcessorPyCore):
 
         # Get voltage using WMIC API
         _process_output = subprocess.check_output(["WMIC", "CPU", "GET", "CurrentVoltage"],
-                                                  text=True).split()
+                                                  text=True, creationflags=subprocess.CREATE_NO_WINDOW).split()
 
         _process_output.remove("CurrentVoltage")
         if len(_process_output) < 0:
@@ -233,11 +225,6 @@ class Sensors(ProcessorPyCore):
     def __adjust_voltage_string(value: int) -> str:
         """ This method will adjust the voltage value and return it as friendly format"""
         return f"{value / 10.0}v"
-
-    def __get_max_clock_speed(self):
-        """ This method will return the cpu max clock speed"""
-        self.__max_clock_speed = int(subprocess.check_output(["WMIC", "CPU", "GET", "MaxClockSpeed"],
-                                                             text=True).split()[-1])
 
     @staticmethod
     def __get_cpu_times():
