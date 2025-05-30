4166b9248da32bfc0ee787226438e8f566cdfc3a
Mr Aymen <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2025-05-31 00:51:25 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,14 @@
+"""
+@author : Aymen Brahim Djelluol
+version  : 0.1
+date : 28.05.2025
+license : MIT License
+
+"""
+
+# IMPORT
+from processorpy.app import __main__
+
+
+if __name__ == "__main__":
+    __main__()
---
+++ run_cli.py
@@ -0,0 +1,14 @@
+"""
+@author : Aymen Brahim Djelloul
+version : 0.1
+date : 29.05.2025
+license : MIT License
+
+"""
+
+# iIMPORTS
+from processorpy.cli import main
+
+
+if __name__ == '__main__':
+    main()
